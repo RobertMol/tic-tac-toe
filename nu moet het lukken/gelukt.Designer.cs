c18d25417de105b61a466c9ae82552b0a68321a3
damianvlegels <djwvlegels@gmail.com>
2020-01-27 09:34:49 +0100

    k

---
+++ nu moet het lukken/gelukt.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace nu_moet_het_lukken
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A01 = new System.Windows.Forms.Button();
+            this.A02 = new System.Windows.Forms.Button();
+            this.A03 = new System.Windows.Forms.Button();
+            this.A10 = new System.Windows.Forms.Button();
+            this.A11 = new System.Windows.Forms.Button();
+            this.A12 = new System.Windows.Forms.Button();
+            this.A20 = new System.Windows.Forms.Button();
+            this.A21 = new System.Windows.Forms.Button();
+            this.A22 = new System.Windows.Forms.Button();
+            this.NGButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Ebutton = new System.Windows.Forms.Button();
+            this.OWin = new System.Windows.Forms.Label();
+            this.Draws = new System.Windows.Forms.Label();
+            this.XWin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // A01
+            // 
+            this.A01.Location = new System.Drawing.Point(12, 14);
+            this.A01.Name = "A01";
+            this.A01.Size = new System.Drawing.Size(107, 73);
+            this.A01.TabIndex = 0;
+            this.A01.UseVisualStyleBackColor = true;
+            this.A01.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A02
+            // 
+            this.A02.Location = new System.Drawing.Point(125, 14);
+            this.A02.Name = "A02";
+            this.A02.Size = new System.Drawing.Size(107, 73);
+            this.A02.TabIndex = 1;
+            this.A02.UseVisualStyleBackColor = true;
+            this.A02.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A03
+            // 
+            this.A03.Location = new System.Drawing.Point(238, 14);
+            this.A03.Name = "A03";
+            this.A03.Size = new System.Drawing.Size(107, 73);
+            this.A03.TabIndex = 2;
+            this.A03.UseVisualStyleBackColor = true;
+            this.A03.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A10
+            // 
+            this.A10.Location = new System.Drawing.Point(11, 93);
+            this.A10.Name = "A10";
+            this.A10.Size = new System.Drawing.Size(107, 73);
+            this.A10.TabIndex = 5;
+            this.A10.UseVisualStyleBackColor = true;
+            this.A10.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A11
+            // 
+            this.A11.Location = new System.Drawing.Point(125, 93);
+            this.A11.Name = "A11";
+            this.A11.Size = new System.Drawing.Size(107, 73);
+            this.A11.TabIndex = 4;
+            this.A11.UseVisualStyleBackColor = true;
+            this.A11.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A12
+            // 
+            this.A12.Location = new System.Drawing.Point(238, 93);
+            this.A12.Name = "A12";
+            this.A12.Size = new System.Drawing.Size(107, 73);
+            this.A12.TabIndex = 3;
+            this.A12.UseVisualStyleBackColor = true;
+            this.A12.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A20
+            // 
+            this.A20.Location = new System.Drawing.Point(12, 172);
+            this.A20.Name = "A20";
+            this.A20.Size = new System.Drawing.Size(107, 73);
+            this.A20.TabIndex = 8;
+            this.A20.UseVisualStyleBackColor = true;
+            this.A20.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A21
+            // 
+            this.A21.Location = new System.Drawing.Point(125, 172);
+            this.A21.Name = "A21";
+            this.A21.Size = new System.Drawing.Size(107, 73);
+            this.A21.TabIndex = 7;
+            this.A21.UseVisualStyleBackColor = true;
+            this.A21.Click += new System.EventHandler(this.button_Click);
+            // 
+            // A22
+            // 
+            this.A22.Location = new System.Drawing.Point(238, 172);
+            this.A22.Name = "A22";
+            this.A22.Size = new System.Drawing.Size(107, 73);
+            this.A22.TabIndex = 6;
+            this.A22.UseVisualStyleBackColor = true;
+            this.A22.Click += new System.EventHandler(this.button_Click);
+            // 
+            // NGButton
+            // 
+            this.NGButton.Location = new System.Drawing.Point(12, 285);
+            this.NGButton.Name = "NGButton";
+            this.NGButton.Size = new System.Drawing.Size(106, 53);
+            this.NGButton.TabIndex = 9;
+            this.NGButton.Text = "New Game";
+            this.NGButton.UseVisualStyleBackColor = true;
+            this.NGButton.Click += new System.EventHandler(this.NGButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 53);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Ebutton
+            // 
+            this.Ebutton.Location = new System.Drawing.Point(238, 285);
+            this.Ebutton.Name = "Ebutton";
+            this.Ebutton.Size = new System.Drawing.Size(106, 53);
+            this.Ebutton.TabIndex = 11;
+            this.Ebutton.Text = "Exit";
+            this.Ebutton.UseVisualStyleBackColor = true;
+            this.Ebutton.Click += new System.EventHandler(this.Ebutton_Click);
+            // 
+            // OWin
+            // 
+            this.OWin.AutoSize = true;
+            this.OWin.Location = new System.Drawing.Point(371, 121);
+            this.OWin.Name = "OWin";
+            this.OWin.Size = new System.Drawing.Size(43, 17);
+            this.OWin.TabIndex = 13;
+            this.OWin.Text = "OWin";
+            // 
+            // Draws
+            // 
+            this.Draws.AutoSize = true;
+            this.Draws.Location = new System.Drawing.Point(371, 200);
+            this.Draws.Name = "Draws";
+            this.Draws.Size = new System.Drawing.Size(47, 17);
+            this.Draws.TabIndex = 14;
+            this.Draws.Text = "Draws";
+            // 
+            // XWin
+            // 
+            this.XWin.AutoSize = true;
+            this.XWin.Location = new System.Drawing.Point(372, 42);
+            this.XWin.Name = "XWin";
+            this.XWin.Size = new System.Drawing.Size(41, 17);
+            this.XWin.TabIndex = 15;
+            this.XWin.Text = "XWin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 349);
+            this.Controls.Add(this.XWin);
+            this.Controls.Add(this.Draws);
+            this.Controls.Add(this.OWin);
+            this.Controls.Add(this.Ebutton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.NGButton);
+            this.Controls.Add(this.A20);
+            this.Controls.Add(this.A21);
+            this.Controls.Add(this.A22);
+            this.Controls.Add(this.A10);
+            this.Controls.Add(this.A11);
+            this.Controls.Add(this.A12);
+            this.Controls.Add(this.A03);
+            this.Controls.Add(this.A02);
+            this.Controls.Add(this.A01);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A01;
+        private System.Windows.Forms.Button A02;
+        private System.Windows.Forms.Button A03;
+        private System.Windows.Forms.Button A10;
+        private System.Windows.Forms.Button A11;
+        private System.Windows.Forms.Button A12;
+        private System.Windows.Forms.Button A20;
+        private System.Windows.Forms.Button A21;
+        private System.Windows.Forms.Button A22;
+        private System.Windows.Forms.Button NGButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Ebutton;
+        private System.Windows.Forms.Label OWin;
+        private System.Windows.Forms.Label Draws;
+        private System.Windows.Forms.Label XWin;
+    }
+}
+
